153749c2806ca2e4e2fbd97ab42aa5b0f2768a57
Kimaru Bethwel <143094784+Kkiget@users.noreply.github.com>
2025-03-18 18:50:42 +0300

    Randomize image order in morphing background effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,13 +59,13 @@ export default function Home() {
       <section className="relative h-screen flex items-center justify-center">
         <style jsx>{`
           @keyframes morph {
-            0% { background-image: url('/images/hero/preview1.jpg'); }
-            16.67% { background-image: url('/images/hero/preview2.jpg'); }
-            33.33%, 50% { background-image: url('/images/hero/farmer.jpg'); }
-            50% { background-image: url('/images/hero/preview3.jpg'); }
-            66.67% { background-image: url('/images/newsletter/Produce2.jpeg'); }
-            83.33% { background-image: url('/images/blog/blog6.jpg'); }
-            100% { background-image: url('/images/blog/blog5.jpg'); }
+            0% { background-image: url('/images/blog/blog6.jpg'); }
+            16.67% { background-image: url('/images/hero/preview3.jpg'); }
+            33.33% { background-image: url('/images/newsletter/Produce2.jpeg'); }
+            50% { background-image: url('/images/hero/preview1.jpg'); }
+            66.67% { background-image: url('/images/blog/blog5.jpg'); }
+            83.33% { background-image: url('/images/hero/preview2.jpg'); }
+            100% { background-image: url('/images/hero/farmer.jpg'); }
           }
           .morphing-background {
             animation: morph 42s infinite linear;
